6556326ebc082955810fd5f958362ad4c5f58c6c
Will Wedmedyk <willwedmedyk@gmail.com>
2022-04-10 22:25:09 -0400

    ww/02prod: add tests for new file structure

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,16 @@
+use actix_web::dev::Server;
 use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+use std::net::TcpListener;
 
 async fn health_check() -> impl Responder {
-    HttpResponse::Ok()
+    HttpResponse::Ok().finish()
 }
 // needs health check still...
 
-pub async fn run() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().route("/health_check", web::get().to(health_check)))
-        .bind("127.0.0.1:8000")?
-        .run()
-        .await
+pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
+    let server = HttpServer::new(|| App::new().route("/health_check", web::get().to(health_check)))
+        .listen(listener)?
+        .run();
+
+    Ok(server)
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,14 @@
+use std::net::TcpListener;
 use zero2prod::run;
+
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    run().await
+    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
+
+    let port = listener.local_addr().unwrap().port();
+    // Bubble up the io::Error if we failed to bind the address
+    // Otherwise call .await on our Server
+    println!("http://127.0.0.1:{}", port);
+
+    run(listener)?.await
 }
